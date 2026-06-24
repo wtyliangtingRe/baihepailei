6239d1d7c1474d49738139ec54bc267eea4a3d18
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-24 21:49:18 +0800

    Temporarily simplify Payload config

--- payload.config.ts
+++ payload.config.ts
@@ -1,40 +1,6 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
-import sharp from 'sharp'
-
-import { Creators } from './src/collections/Creators'
-import { Media } from './src/collections/Media'
-import { Rules } from './src/collections/Rules'
-import { Tags } from './src/collections/Tags'
-import { Terms } from './src/collections/Terms'
-import { Users } from './src/collections/Users'
-import { Warnings } from './src/collections/Warnings'
-import { Works } from './src/collections/Works'
-
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    meta: {
-      titleSuffix: '- Baihepailei',
-    },
-  },
-  collections: [Users, Media, Works, Creators, Terms, Warnings, Tags, Rules],
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URL,
-    },
-  }),
-  editor: lexicalEditor(),
+  collections: [],
   secret: process.env.PAYLOAD_SECRET || '',
-  sharp,
-  typescript: {
-    outputFile: path.resolve(dirname, 'src/payload-types.ts'),
-  },
 })
