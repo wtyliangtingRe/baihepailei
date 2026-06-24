a5bde9cbebc6d98d5a968810da06198be6191e96
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-24 21:54:45 +0800

    Use stable collections for first boot

--- payload.config.ts
+++ payload.config.ts
@@ -1,27 +1,15 @@
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 
 import { Creators } from './src/collections/Creators'
-import { Entries } from './src/collections/Entries'
 import { Media } from './src/collections/Media'
 import { Rules } from './src/collections/Rules'
 import { Terms } from './src/collections/Terms'
 import { Users } from './src/collections/Users'
 
-const dbUrl = process.env['DATABASE_URL']
-const appSecret = process.env['PAYLOAD_SECRET'] || ''
-
 export default buildConfig({
   admin: {
     user: Users.slug,
   },
-  collections: [Users, Media, Entries, Creators, Terms, Rules],
-  db: postgresAdapter({
-    pool: {
-      connectionString: dbUrl,
-    },
-  }),
-  editor: lexicalEditor(),
-  secret: appSecret,
+  collections: [Users, Media, Creators, Terms, Rules],
+  secret: process.env['PAYLOAD_SECRET'] || '',
 })
