b92b56092e2172d38e2210dcd0bec4d7762c8e6b
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-24 21:51:06 +0800

    Add Lexical editor to Payload config

--- payload.config.ts
+++ payload.config.ts
@@ -1,4 +1,5 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { buildConfig } from 'payload'
 
 import { Creators } from './src/collections/Creators'
@@ -21,5 +22,6 @@ export default buildConfig({
       connectionString: dbUrl,
     },
   }),
+  editor: lexicalEditor(),
   secret: appSecret,
 })
