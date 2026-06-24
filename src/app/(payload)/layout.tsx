341545654e6783f5e6ff36b0acf440869002b550
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-24 21:52:51 +0800

    Add Payload app layout route

---
+++ src/app/(payload)/layout.tsx
@@ -0,0 +1,28 @@
+import config from '@payload-config'
+import '@payloadcms/next/css'
+import type { ServerFunctionClient } from 'payload'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import React from 'react'
+
+import { importMap } from './admin/importMap.js'
+
+type Args = {
+  children: React.ReactNode
+}
+
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
+export default function PayloadLayout({ children }: Args) {
+  return (
+    <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+      {children}
+    </RootLayout>
+  )
+}
