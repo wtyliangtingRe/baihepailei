959183e067c1c1936b3d28b4dd9423d7162f91e5
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-25 14:53:12 +0800

    Add rules index page

---
+++ src/app/(frontend)/rules/page.tsx
@@ -0,0 +1,5 @@
+import CollectionIndexPage from '../_components/CollectionIndexPage'
+
+export default function RulesIndexPage() {
+  return <CollectionIndexPage collection="rules" description="Rule entries from the Lite search index." eyebrow="rules" title="Rules" />
+}
