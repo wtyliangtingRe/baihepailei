ade7b8b36f639c575de24bb9b90dc6938ce3e995
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-06-25 14:29:09 +0800

    Use detail index on work pages

--- src/app/(frontend)/works/[slug]/page.tsx
+++ src/app/(frontend)/works/[slug]/page.tsx
@@ -1,7 +1,9 @@
 import { notFound } from 'next/navigation'
 
+import DetailIndexDetail from '../../_components/DetailIndexDetail'
 import MissingSearchIndex from '../../_components/MissingSearchIndex'
 import SearchIndexDetail from '../../_components/SearchIndexDetail'
+import { findDetailItem } from '../../_lib/detail-index'
 import { findSearchItem, readSearchIndex } from '../../_lib/search-index'
 
 type Args = {
@@ -10,10 +12,15 @@ type Args = {
 
 export default async function WorkDetailPage({ params }: Args) {
   const { slug } = await params
+  const decodedSlug = decodeURIComponent(slug)
+
+  const detailItem = findDetailItem('works', decodedSlug)
+  if (detailItem) return <DetailIndexDetail item={detailItem} />
+
   const index = readSearchIndex()
   if (!index) return <MissingSearchIndex />
 
-  const item = findSearchItem('works', decodeURIComponent(slug))
+  const item = findSearchItem('works', decodedSlug)
   if (!item) notFound()
 
   return <SearchIndexDetail item={item} />
