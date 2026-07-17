050d0563fd959502420ecd7356c8f1af5846c680
衛藤天音 <95459097+wtyliangtingRe@users.noreply.github.com>
2026-07-17 08:33:20 +0800

    Add src/app/(frontend)/me/personnel/page.tsx

---
+++ src/app/(frontend)/me/personnel/page.tsx
@@ -0,0 +1,5 @@
+import PersonnelClient from '../../_components/PersonnelClient'
+
+export default function PersonnelPage() {
+  return <main className="page personnel-page"><PersonnelClient /></main>
+}
