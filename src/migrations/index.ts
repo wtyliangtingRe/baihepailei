3ea530cb342bfab5c126e94498d2d94c98413aa0
衛藤天音 <wty1123581321@gmail.com>
2026-07-23 22:19:11 +0800

    Add Radar public conclusions migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,7 @@
 import * as migration_20260718_072813_existing_schema_baseline_v01 from './20260718_072813_existing_schema_baseline_v01';
 import * as migration_20260718_072843_stewardship_notices_v01 from './20260718_072843_stewardship_notices_v01';
+import * as migration_20260723_141905_current_schema_baseline_before_radar_public_v01 from './20260723_141905_current_schema_baseline_before_radar_public_v01';
+import * as migration_20260723_141908_radar_public_conclusions_v01 from './20260723_141908_radar_public_conclusions_v01';
 
 export const migrations = [
   {
@@ -10,6 +12,16 @@ export const migrations = [
   {
     up: migration_20260718_072843_stewardship_notices_v01.up,
     down: migration_20260718_072843_stewardship_notices_v01.down,
-    name: '20260718_072843_stewardship_notices_v01'
+    name: '20260718_072843_stewardship_notices_v01',
+  },
+  {
+    up: migration_20260723_141905_current_schema_baseline_before_radar_public_v01.up,
+    down: migration_20260723_141905_current_schema_baseline_before_radar_public_v01.down,
+    name: '20260723_141905_current_schema_baseline_before_radar_public_v01',
+  },
+  {
+    up: migration_20260723_141908_radar_public_conclusions_v01.up,
+    down: migration_20260723_141908_radar_public_conclusions_v01.down,
+    name: '20260723_141908_radar_public_conclusions_v01'
   },
 ];
