2f44adcfbd4804421149745bb88da455292d6c8a
衛藤天音 <wty1123581321@gmail.com>
2026-08-02 14:20:19 +0800

    Add public evidence role migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -6,6 +6,7 @@ import * as migration_20260801_101546_current_schema_baseline_before_radar_publi
 import * as migration_20260801_101551_radar_public_records_v01 from './20260801_101551_radar_public_records_v01';
 import * as migration_20260802_030535_radar_public_ratings_v01 from './20260802_030535_radar_public_ratings_v01';
 import * as migration_20260802_045057_radar_public_record_fact_value_text_v01 from './20260802_045057_radar_public_record_fact_value_text_v01';
+import * as migration_20260802_062015_radar_public_record_evidence_role_v01 from './20260802_062015_radar_public_record_evidence_role_v01';
 
 export const migrations = [
   {
@@ -46,6 +47,11 @@ export const migrations = [
   {
     up: migration_20260802_045057_radar_public_record_fact_value_text_v01.up,
     down: migration_20260802_045057_radar_public_record_fact_value_text_v01.down,
-    name: '20260802_045057_radar_public_record_fact_value_text_v01'
+    name: '20260802_045057_radar_public_record_fact_value_text_v01',
+  },
+  {
+    up: migration_20260802_062015_radar_public_record_evidence_role_v01.up,
+    down: migration_20260802_062015_radar_public_record_evidence_role_v01.down,
+    name: '20260802_062015_radar_public_record_evidence_role_v01'
   },
 ];
